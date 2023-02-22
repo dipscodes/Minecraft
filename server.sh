d3fb4b0758837a9de87929e127ea7deb124ecf92
Deep Khamaru <deepkumarkhamaru@google.com>
2023-02-22 15:32:07 +0530

    generalized to current sudo user

--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/mcadmin/ # needs to be generalilzed
+cd $HOME
 rm -r LyadhCraft 
 mkdir LyadhCraft
 cd LyadhCraft
@@ -20,10 +20,10 @@ unzip SERVER_MODS.zip
 rm SERVER_MODS.zip
 cd ..
 
-cp /home/mcadmin/runmcserver.sh /home/mcadmin/LyadhCraft/runmcserver.sh
-chmod +x /home/mcadmin/LyadhCraft/runmcserver.sh
-cp /home/mcadmin/bootup.sh /home/mcadmin/LyadhCraft/bootup.sh
-chmod +x /home/mcadmin/LyadhCraft/bootup.sh
+cp ./runmcserver.sh $HOME/LyadhCraft/runmcserver.sh
+chmod +x $HOME/LyadhCraft/runmcserver.sh
+cp ./bootup.sh $HOME/LyadhCraft/bootup.sh
+chmod +x $HOME/LyadhCraft/bootup.sh
 
 chown mcadmin:mcadmin -R .
 
@@ -32,7 +32,7 @@ service autostart stop
 systemctl disable autostart.service
 rm /etc/systemd/system/autostart.service
 
-cp /home/mcadmin/autostart.service /etc/systemd/system/autostart.service
+cp ./autostart.service /etc/systemd/system/autostart.service
 systemctl daemon-reload
 systemctl enable autostart.service # enables the service for restart
 service autostart status
@@ -44,7 +44,7 @@ service runmcserver stop
 systemctl disable runmcserver.service
 rm /etc/systemd/system/runmcserver.service
 
-cp /home/mcadmin/runmcserver.service /etc/systemd/system/runmcserver.service
+cp ./runmcserver.service /etc/systemd/system/runmcserver.service
 systemctl daemon-reload
 systemctl enable runmcserver.service # enables the server for restart
 service runmcserver status
