b82ded473190e80d87d4fb5091b3d4a1368c349f
dipscodes <44461133+dipscodes@users.noreply.github.com>
2023-02-23 20:03:33 +0530

    Update runmcserver.sh

--- runmcserver.sh
+++ runmcserver.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/usr/bin/java -Xmx6G -jar /home/mcadmin/LyadhCraft/server.jar nogui
+tmux new-session -s "java" -d
+tmux send-keys -t "java" "cd /home/mcadmin/LyadhCraft" Enter
+tmux send-keys -t "java" "java -Xmx6G -jar ./server.jar nogui" Enter
