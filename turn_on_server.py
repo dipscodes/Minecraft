994233d1df0aa21ed0a229ff541ebf4a361a5667
Deep Kumar Khamaru <deepkumarkhamaru@gmail.com>
2023-03-02 14:27:29 +0530

    working

--- turn_on_server.py
+++ turn_on_server.py
@@ -1,8 +1,9 @@
 import requests
+import logging
 
 
 droplet_id = 342253375
-DIGITALOCEAN_TOKEN = "dop_v1_da76d577ee9beb54a906cdc8ab93ff91d8fdb2b7b571848eda221248085cac6c"
+DIGITALOCEAN_TOKEN = "dop_v1_909222ed2262c05a0f80340e44d8f83927e62efcb34f546ae20c145aac117f2d"
 
 get_url = "https://api.digitalocean.com/v2/droplets?page=1"
 post_url = f"https://api.digitalocean.com/v2/droplets/{droplet_id}/actions"
@@ -18,10 +19,11 @@ list_of_droplets = requests.get(url=get_url, headers=headers).json()
 
 try:
     if list_of_droplets['id'] == "Unauthorized":
-        print("The Key doesn't work.")
+        logging.error("The Key doesn't work.")
 except KeyError:
     list_of_droplets = list_of_droplets["droplets"]
     for droplet in list_of_droplets:
         if droplet["id"] == droplet_id:
             if droplet["status"] == "off":
                 requests.post(url=post_url, json=data, headers=headers)
+                logging.info("Power on successful.")
