07e237aa98a7f55cf70d4d5cdff33a95e7fd8d8d
Deep Kumar Khamaru <deepkumarkhamaru@gmail.com>
2023-03-02 11:44:09 +0530

    updated for potential token error

--- turn_on_server.py
+++ turn_on_server.py
@@ -1,7 +1,8 @@
 import requests
 
+
 droplet_id = 342253375
-DIGITALOCEAN_TOKEN = "Bearer dop_v1_da76d577ee9beb54a906cdc8ab93ff91d8fdb2b7b571848eda221248085cac6c"
+DIGITALOCEAN_TOKEN = "dop_v1_da76d577ee9beb54a906cdc8ab93ff91d8fdb2b7b571848eda221248085cac6c"
 
 get_url = "https://api.digitalocean.com/v2/droplets?page=1"
 post_url = f"https://api.digitalocean.com/v2/droplets/{droplet_id}/actions"
@@ -11,11 +12,18 @@ headers = {
     "Authorization": f"Bearer {DIGITALOCEAN_TOKEN}"
 }
 
-data = {"type":"power_on"}
+print(headers)
 
-list_of_droplets = requests.get(url=get_url, headers=headers).json()["droplets"]
+data = {"type": "power_on"}
 
-for droplet in list_of_droplets:
-    if droplet["id"] == droplet_id:
-        if droplet["status"] == "off":
-            requests.post(url=post_url, json=data, headers=headers)
+list_of_droplets = requests.get(url=get_url, headers=headers).json()
+
+try:
+    if list_of_droplets['id'] == "Unauthorized":
+        print("The Key doesn't work.")
+except KeyError:
+    list_of_droplets = list_of_droplets["droplets"]
+    for droplet in list_of_droplets:
+        if droplet["id"] == droplet_id:
+            if droplet["status"] == "off":
+                requests.post(url=post_url, json=data, headers=headers)
