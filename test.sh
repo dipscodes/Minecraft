7280e787dd2f61aadd1e60742f0354b0ba96cd53
Deep Khamaru <deepkumarkhamaru@google.com>
2023-03-06 16:00:48 +0530

    oops

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# timedatectl set-timezone Asia/Kolkata
+timedatectl set-timezone Asia/Kolkata
 ct=$(date +%H%M)
 cd=$(date +%u)
 
 if ([ "$ct" -gt 0200 ] && [ "$ct" -lt 1000 ]) || ([ $cd -gt 0 ] && [ $cd -lt 5 ]) || ([ "$ct" -lt 1600 ] && [ $cd == 5 ]); then
-    echo Server illegally started at $(date) >> /home/dips/VSCode/Minecraft/server_1.log
-    # shutdown now
+    echo Server illegally started at $(date) >> /home/mcadmin/server.log
+    shutdown now
     exit 0
 fi
 
-# shutdown 02:00
-echo Server normally started at $(date) >> /home/dips/VSCode/Minecraft/server_1.log
+shutdown 02:00
+echo Server normally started at $(date) >> /home/mcadmin/server.log
